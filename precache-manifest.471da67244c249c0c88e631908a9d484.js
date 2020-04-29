fda3bfe3d06adc3faaad114dd7e2b196c4ac1bb0
Miikka Holkeri <13796320+holkeri@users.noreply.github.com>
2020-04-28 21:21:07 -0700

    Updates

---
+++ precache-manifest.471da67244c249c0c88e631908a9d484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d58087160bddf1db03e089e1c789e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "366480f5251e7992a5d9",
+    "url": "/static/css/main.015e9ab4.chunk.css"
+  },
+  {
+    "revision": "1802ba80bef9b738c91d",
+    "url": "/static/js/2.0d3a5505.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d3a5505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366480f5251e7992a5d9",
+    "url": "/static/js/main.559dee40.chunk.js"
+  },
+  {
+    "revision": "5548fcc06e0e714a55ad",
+    "url": "/static/js/runtime-main.c6a8106c.js"
+  }
+]);
