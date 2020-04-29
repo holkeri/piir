344bd12d87b22a13dd3e942610d086fa08e7055a
Miikka Holkeri <miikka@smartly.io>
2020-04-28 19:07:48 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useState } from 'react';
 import './App.css';
+import content from './content.json';
+import { useTransition, animated } from 'react-spring'
+
+// http://reactcommunity.org/react-transition-group/switch-transition
+
+const spliceRandomElement = array => array.splice(Math.floor(array.length * Math.random()), 1);
 
 function App() {
+  const [language, setLanguage] = useState('fi');
+  const [phrases, setPhrases] = useState([]);
+  const [phrase, setPhrase] = useState();
+
+  const changeLanguage = event => setLanguage(event.target.value)
+  const getNewPhrase = () => {
+    const newPhrases = phrases.length ? [...phrases] : [...content];
+    const newPhrase = spliceRandomElement(newPhrases);
+    setPhrase(newPhrase);
+    setPhrases(newPhrases);
+  };
+  const clearPhrase = () => setPhrase(undefined);
+
+  const transitions = useTransition(phrase && phrase.map(p => p[language]), null, {
+    from: { position: 'absolute', opacity: 0, transform: 'scale3d(0, 0, 0)' },
+    enter: { opacity: 1, transform: 'scale3d(1, 1, 1)' },
+    leave: { opacity: 0, transform: 'scale3d(0, 0, 0)' },
+    config: (item, state) => (state === 'leave' ? { friction: 40, tension: 360 } : { friction: 12, tension: 280 })
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <select className="button" value={language} onChange={changeLanguage}>
+          <option value="fi">🇫🇮</option>
+          <option value="en">🇬🇧</option>
+        </select>
+      </div>
+      <div className="main">
+        {transitions.map(({ item, key, props }) => <animated.p style={props} key={key} className="phrase">{item}</animated.p>)}
+      </div>
+      <div className="footer">
+        {phrase && <button className="button" onClick={clearPhrase}>❌</button>}
+        <button className="button" onClick={getNewPhrase}>✨</button>
+      </div>
     </div>
   );
 }
