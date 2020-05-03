b08610537e160cdc583572ff31a9ae890ae4a6e1
Miikka Holkeri <13796320+holkeri@users.noreply.github.com>
2020-05-02 19:52:41 -0700

    hidden multidevice support

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import content from './content.json';
-import { SwitchTransition, CSSTransition } from "react-transition-group";
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 
 const spliceRandomElement = array => array.splice(Math.floor(array.length * Math.random()), 1)[0];
 
-function App() {
+const getAllPhrases = () => {
+  const playerParams = window.location.href.split('#')[1]?.split('/');
+  if (playerParams?.length === 2) {
+    const playerNum = parseInt(playerParams[0]);
+    const numPlayers = parseInt(playerParams[1]);
+    return content.filter((p, i) => i % numPlayers + 1 === playerNum);
+  }
+  return [...content];
+}
+
+const App = () => {
   const [language, setLanguage] = useState('fi');
   const [phrases, setPhrases] = useState([]);
   const [phrase, setPhrase] = useState();
 
   const toggleLanguage = () => setLanguage(language === 'fi' ? 'en' : 'fi');
   const getNewPhrase = () => {
-    const newPhrases = phrases.length ? [...phrases] : [...content];
+    const newPhrases = phrases.length ? [...phrases] : getAllPhrases();
     const newPhrase = spliceRandomElement(newPhrases);
     setPhrase(newPhrase);
     setPhrases(newPhrases);
